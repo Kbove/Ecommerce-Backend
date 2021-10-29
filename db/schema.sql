f2849d5f46f49041fbb2928ebc1252f407d0062e
Kbove <kbove94@gmail.com>
2021-10-28 22:17:55 -0700

    pushing

--- db/schema.sql
+++ db/schema.sql
@@ -4,34 +4,34 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-USE ecommerce_db
+USE ecommerce_db;
 
-CREATE TABLE category (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-category_name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE category (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- category_name VARCHAR(30) NOT NULL
+-- );
 
-CREATE TABLE product (
-id int not null primary key auto_increment,
-product_name varchar(30) not null,
-price decimal(3,2) not null,
-stock int not null default 10,
-category_id int, 
-foreign key (category_id)
-references category(id) 
-);
+-- CREATE TABLE product (
+-- id int not null primary key auto_increment,
+-- product_name varchar(30) not null,
+-- price decimal(3,2) not null,
+-- stock int not null default 10,
+-- category_id int, 
+-- FOREIGN KEY (category_id)
+-- REFERENCES category(id) 
+-- );
 
-CREATE TABLE tag (
-id int not null primary key auto_increment,
-tag_name varchar(30)
-);
+-- CREATE TABLE tag (
+-- id int not null primary key auto_increment,
+-- tag_name VARCHAR(30)
+-- );
 
-CREATE TABLE producttag (
-id int not null primary key auto_increment,
-product_id int,
-tag_id int,
-foreign key (tag_id)
-references tag(id),
-foreign key (product_id)
-references product(id)
-);
+-- CREATE TABLE producttag (
+-- id int not null primary key auto_increment,
+-- product_id int,
+-- tag_id int,
+-- foreign key (tag_id)
+-- references tag(id),
+-- foreign key (product_id)
+-- references product(id)
+-- );
--- seeds/index.js
+++ seeds/index.js
@@ -8,8 +8,8 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  // await seedCategories();
+  // console.log('\n----- CATEGORIES SEEDED -----\n');
 
   await seedProducts();
   console.log('\n----- PRODUCTS SEEDED -----\n');
@@ -17,8 +17,8 @@ const seedAll = async () => {
   await seedTags();
   console.log('\n----- TAGS SEEDED -----\n');
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  // await seedProductTags();
+  // console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
   process.exit(0);
 };
